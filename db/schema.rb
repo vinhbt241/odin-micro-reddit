2f9fc7d63ad68e341a61d5a7659776b09762b45d
Vinh <vinhbuith32412000@gmail.com>
2022-03-19 10:57:26 +0700

    Add Post model and associated it with User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_19_030354) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_19_033247) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
